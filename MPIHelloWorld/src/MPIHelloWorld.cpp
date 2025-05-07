c4d49bd3b6d7fff468fbaeac1e6525aeeded709f
inf-eth <infinity.ethereal@gmail.com>
2025-05-08 00:30:36 +0100

    - Changed whiteespace to tab.

--- MPIHelloWorld/src/MPIHelloWorld.cpp
+++ MPIHelloWorld/src/MPIHelloWorld.cpp
@@ -14,10 +14,10 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 	// Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-	
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	MPI_Get_processor_name(processor_name, &name_len);
+
 	cout << "Hello from processor " << processor_name << " with rank " << world_rank << " of " << world_size << endl;
 
 	MPI_Finalize();
